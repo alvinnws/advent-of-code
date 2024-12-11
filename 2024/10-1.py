ca0384b7afc5131e32ed55616124eefb3c019d2c
Alvin Ng <alvinnws4546@gmail.com>
2024-12-11 13:07:59 +0800

    Solved day 10

---
+++ 2024/10-1.py
@@ -0,0 +1,42 @@
+f = open('inputs/10.txt')
+
+total = 0
+
+ls = []
+for line in f:
+    row = []
+    for c in line.strip():
+        row.append(c)
+    ls.append(row)
+
+sps = []
+
+for i in range(len(ls)):
+    for j in range(len(ls[0])):
+        if ls[i][j] == '0':
+            sps.append((i, j))
+
+def explore(ls, pos, explored=set()):
+    dirs = [
+        (0, 1),
+        (0,-1),
+        (1, 0),
+        (-1,0)
+    ]
+    if ls[pos[0]][pos[1]] == "9":
+        explored.add(pos)
+    else:
+        for i in dirs:
+            if 0 <= pos[0]+i[0] and pos[0]+i[0] < len(ls) and 0 <= pos[1]+i[1] and pos[1]+i[1] < len(ls[0]):
+                # print((pos[0]+i[0], pos[1]+i[1]), int(ls[pos[0]+i[0]][pos[1]+i[1]]), int(ls[pos[0]][pos[1]]) + 1, int(ls[pos[0]+i[0]][pos[1]+i[1]]) == int(ls[pos[0]][pos[1]]) + 1)
+                if int(ls[pos[0]+i[0]][pos[1]+i[1]]) == int(ls[pos[0]][pos[1]]) + 1:
+                    # print((pos[0]+i[0], pos[1]+i[1]))
+                    explore(ls, (pos[0]+i[0], pos[1]+i[1]), explored)
+    return explored
+
+for sp in sps:
+    total += len(explore(ls, sp, set()))
+
+print(total)
+
+f.close()
--- 2024/README.md
+++ 2024/README.md
@@ -1,21 +1,25 @@
 # 2024
 
 ## Timings
-1-1 - 03min24s  
-1-2 - 02min22s  
-2-1 - 22min53s  
-2-2 - 37min00s  
-3-1 - 06min14s  
-3-2 - 02min20s  
-4-1 - 29min46s  
-4-2 - 13min00s  
-5-1 - 19min56s  
-5-2 - 15min35s  
-6-1 - 33min42s  
-6-2 - WIP  
-7-1 - 22min14s  
-7-2 - 02min04s  
-8-1 - 25min40s  
-8-2 - 12min23s  
-9-1 - 11min57s (675th global)  
-9-2 - 01hrs10m
+| Day-Part | Timing | 
+|----------|--------| 
+| 1-1  |  03min24s  |  
+| 1-2  |  02min22s  |  
+| 2-1  |  22min53s  |  
+| 2-2  |  37min00s  |  
+| 3-1  |  06min14s  |  
+| 3-2  |  02min20s  |  
+| 4-1  |  29min46s  |  
+| 4-2  |  13min00s  |  
+| 5-1  |  19min56s  |  
+| 5-2  |  15min35s  |  
+| 6-1  |  33min42s  |  
+| 6-2  |  WIP  |  
+| 7-1  |  22min14s  |  
+| 7-2  |  02min04s  |  
+| 8-1  |  25min40s  |  
+| 8-2  |  12min23s  |  
+| 9-1  |  11min57s (675th global)  |  
+| 9-2  |  01hrs10m  |  
+| 10-1 |  25mins40s  |  
+| 10-2 |  01mins52s  |  
