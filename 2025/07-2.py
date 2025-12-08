b41b14a03067482ac6fb0abc61fc12f92850c199
Alvin Ng <99571694+alvinnws@users.noreply.github.com>
2025-12-08 11:08:49 +0800

    Day 7

---
+++ 2025/07-2.py
@@ -0,0 +1,27 @@
+arr = [[c for c in line.strip()] for line in open('inputs/7.txt')]
+
+for j in range(len(arr[0])):
+    if arr[0][j] == 'S':
+        start = (1,j)
+        break
+
+
+def beam(arr, pos, hist):
+    if pos in hist:
+        return hist[pos]
+    if pos[0] == len(arr):
+        hist[pos] = 0
+        return hist[pos]
+    if arr[pos[0]][pos[1]] == '.':
+        hist[pos] = beam(arr, (pos[0]+1, pos[1]), hist)
+        return hist[pos]
+    else:
+        left = beam(arr, (pos[0], pos[1]-1), hist)
+        right = beam(arr, (pos[0], pos[1]+1), hist)
+        hist[pos] = left + right + 1
+        return hist[pos]
+    hist[pos] = 0
+    return 0
+
+hist = {}
+print(beam(arr, start, hist)+1)
