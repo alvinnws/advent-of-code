211c73a519ecc9fcf0537fd6166f622d014b1837
Alvin Ng <99571694+alvinnws@users.noreply.github.com>
2025-12-17 16:01:21 +0800

    Reduced lines by modulo and rl dict

--- 2025/01-2.py
+++ 2025/01-2.py
@@ -1,23 +1,10 @@
 f = open('inputs/1.txt')
 
-zeroes = 0
-current = 50
+zeroes, current, rl = 0, 50, {"L": -1, "R": 1}
 
 for line in f:
-    if line[0] == "L":
-        for i in range(int(line.strip()[1:])):
-            current -= 1
-            if current == -1:
-                current = 99
-            if current == 0:
-                zeroes += 1
-    else:
-        for i in range(int(line.strip()[1:])):
-            current += 1
-            if current == 100:
-                current = 0
-            if current == 0:
-                zeroes += 1
-
+    for i in range(int(line.strip()[1:])):
+        current = (current + rl[line[0]]) % 100
+        if current == 0: zeroes += 1
 
 print(zeroes)
