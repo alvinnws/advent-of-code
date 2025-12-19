ce2d2c7adc2635f9f763738be4cf91db255cbb60
Alvin Ng <99571694+alvinnws@users.noreply.github.com>
2025-12-19 21:20:13 +0800

    Optimised

--- 2025/OCaml/lib/day01.ml
+++ 2025/OCaml/lib/day01.ml
@@ -3,33 +3,16 @@ let rec rl ic n =
     let line = input_line ic in
     let direction = (String.get line 0) in
     let rotation = int_of_string (String.sub line 1 ((String.length line)-1)) in
-    if direction = 'L' then
-      let res = (n-rotation) in 
-      if res < 0 then
-        let res2 = ((res mod 100) + 100) in
-        if res2 = 100 then 
-          1+(rl ic 0)
-        else
-          rl ic res2
-      else if res = 0 then
-        1+(rl ic 0)
-      else
-        rl ic res
+    let res = if direction = 'L' then n-rotation else n+rotation in
+    if res mod 100 < 0 then
+      rl ic ((res mod 100) + 100)
+    else if res mod 100 = 0 then
+      1 + (rl ic 0)
     else
-      let res = (n+rotation) in 
-      if res > 100 then
-        let res2 = (res mod 100) in
-          if res2 = 0 then 
-            1+(rl ic 0)
-          else
-            rl ic res2
-      else if res = 100 then
-        1+(rl ic 0)
-      else
-        rl ic res
+      rl ic (res mod 100)
   with _e ->
     close_in ic;
     0
-    
+
 let part1 file =
   rl (open_in file) 50
